ddb167206fdf3b8213daa7161f6a5bfef02a0b4e
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-07-13 00:49:44 +0300

    main elements and two viewController are placed

---
+++ logInApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  logInApp
+//
+//  Created by Константин Прокофьев on 13.07.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
