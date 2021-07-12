c680d06246e0683e6fd23a0c8ffbc0b395334fae
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-07-12 23:51:34 +0300

    Initial Commit

---
+++ logInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  logInApp
+//
+//  Created by Константин Прокофьев on 12.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
